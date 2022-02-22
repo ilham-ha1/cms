9f2bcd37b15942be8fa77b4c2abb282ec05b92d1
ilhamhakim1205 <m.ilhamhakim@gmail.com>
2022-02-22 15:09:13 +0700

    Practicum 2 Week 2 FIX

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo "Muhammad Ilham El Hakim <br>";
+        echo "2041720162";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
-
+// use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,7 +29,11 @@ use App\Http\Controllers\PageController;
 //     echo "This is Article Pages with ID: ".$id;
 // });
 //Practicum 2
-Route::get('/',[PageController::class, 'index']);
-Route::get('/about',[PageController::class,'about']);
-Route::get('/articles/{id}',[PageController::class,'articles']);
+// Route::get('/',[PageController::class, 'index']);
+// Route::get('/about',[PageController::class,'about']);
+// Route::get('/articles/{id}',[PageController::class,'articles']);
+
+Route::get('/',[HomeController::class, 'index']);
+Route::get('/about',[AboutController::class,'about']);
+Route::get('/articles/{id}',[ArticleController::class,'articles']);
 
