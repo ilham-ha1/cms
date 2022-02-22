138090e4b0239486409bfcbf6fef52b11a59d72a
ilhamhakim1205 <m.ilhamhakim@gmail.com>
2022-02-22 14:24:20 +0700

    Practicum 1 Week 2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Practicum 1
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hi! Welcome To Laravel";
 });
+Route::get('/about', function(){
+    echo "Muhammad Ilham El Hakim <br>";
+    echo "2041720162 <br>";
+    echo "TI-2I";
+});
+Route::get('/articles/{id}', function($id){
+    echo "This is Article Pages with ID: ".$id;
+});
+//Practicum 2
+
+
